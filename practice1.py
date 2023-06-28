ed098164cec8a2e2c4b8d6e7d441c132640da9f6
Droy <royd4405aoi@gmail.com>
2023-06-27 17:13:31 -0700

    practice1.py

---
+++ practice1.py
@@ -0,0 +1,14 @@
+class Employee:
+
+    def __init__(self, name, no):
+        self.no = no
+        self.name = name
+
+    def display(self):
+        print(self.name, self.no)
+
+
+emp1 = Employee("John",11)
+emp2 = Employee("David", 12)
+emp1.display()
+emp2.display()
